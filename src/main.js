3aedcfbd4b286af4b144603885ee62deac8de0c1
dharmendra.s <dharmendra.s@BBMDVDHARMENDRAS.local>
2018-04-10 14:31:23 +0530

    updated code in js file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 /**
  * The Entry point for application
  */
+
+ import { getMessage } from "./modules/message.js"
+
+(function() {
+    document.getElementById("app").innerHTML = getMessage();
+})();
---
+++ src/modules/message.js
@@ -0,0 +1,8 @@
+
+const message ="Hello world"
+
+export const getMessage = () => {
+    return `<div>
+                <h1>${message}</h1>
+            </div>`
+}
